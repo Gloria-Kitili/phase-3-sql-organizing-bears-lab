49cb238c5034de595239a839448709eec429e9d6
root <gloria.kitili@student.moringaschool.com>
2023-05-30 18:54:15 +0300

    create ,insert .sql

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears(
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    sex VARCHAR(10),
+    color VARCHAR(255),
+     temperament VARCHAR(255),
+     alive BOOLEAN 
+
+);
